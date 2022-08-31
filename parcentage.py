b13bfe0d392c995f9cb6d0a2e3072e4f761d6626
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-08-31 07:35:09 +0600

    Update: parcentage.py "set variables"

--- parcentage.py
+++ parcentage.py
@@ -1,1 +1,10 @@
 ## now coin price is 100 taka. 30 minust ago its down to 80 taka and 10 minuts ago its higest price was 120 taka how much percentage the price goesup and down?
+
+
+current_price = 100
+
+high_price = 120
+
+low_price = 80
+
+
